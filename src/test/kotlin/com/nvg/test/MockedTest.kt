db3ed73cfcdf6f6f03cf0ea0e8eda410c3b142e0
Huy Hua <huyhua@nhatvietgroup.com.vn>
2019-07-29 10:34:23 +0700

    fix assertion

--- src/test/kotlin/com/nvg/test/MockedTest.kt
+++ src/test/kotlin/com/nvg/test/MockedTest.kt
@@ -6,15 +6,18 @@ import com.nvg.utils.loadFile
 import io.restassured.RestAssured
 import io.restassured.RestAssured.given
 import io.restassured.builder.RequestSpecBuilder
+import io.restassured.http.ContentType
 import io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchema
 import org.hamcrest.CoreMatchers.*
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.api.extension.ExtendWith
 import ru.lanwen.wiremock.ext.WiremockResolver
 import ru.lanwen.wiremock.ext.WiremockResolver.*
 import ru.lanwen.wiremock.ext.WiremockUriResolver
 import ru.lanwen.wiremock.ext.WiremockUriResolver.*
+import java.lang.AssertionError
 
 @ExtendWith(
     WiremockResolver::class,
@@ -38,17 +41,17 @@ class MockedTest: TestBase() {
                 .body("name", `is`("Toutes les rubriques"))
     }
 
-//    @Test
-//
-//    fun `test exception is thrown when wrong thing happened`() {
-//
-//        given().baseUri("https://api-stage.anibis.ch/v1")
-//            .port(8080)
-//            .When()
-//                .get("/fr/categories")
-//            .then()
-//                .contentType(ContentType.XML)
-//    }
+    @Test
+    fun `test exception is thrown when wrong thing happened`() {
+        assertThrows<AssertionError> {
+            given().baseUri("https://api-stage.anibis.ch/v1")
+                .port(8080)
+                .When()
+                .get("/fr/categories")
+                .then()
+                .contentType(ContentType.XML)
+        }
+    }
 
 
     @Test
